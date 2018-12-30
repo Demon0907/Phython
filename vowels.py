d6c4f8810e3d792bbec3bdaa09b334a9c2abd89c
Demon0907 <39375128+Demon0907@users.noreply.github.com>
2018-12-30 12:08:09 +0530

    Update vowels.py

--- vowels.py
+++ vowels.py
@@ -1,7 +1,7 @@
-list=["a","e","i","o","u","A","E","I","O","U"]
-x=raw_input("")
-print(x)
-if (x in list):
+l=["a","e","i","o","u","A","E","I","O","U"]
+str=raw_input("")
+print(str)
+if (str in l):
     print ("vowel")
 else:
     print ("consonant")
